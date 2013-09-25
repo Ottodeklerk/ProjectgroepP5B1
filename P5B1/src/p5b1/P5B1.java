5b91bd2c8d6da4590e7a78e192ef5e26773cae29
O. De Klerk <sonobull@hotmail.com>
2013-09-25 12:14:06 +0200

    Commit

--- P5B1/src/p5b1/P5B1.java
+++ P5B1/src/p5b1/P5B1.java
@@ -16,5 +16,6 @@ public class P5B1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hoi");
+        System.out.println("Hoi");
     }
 }
